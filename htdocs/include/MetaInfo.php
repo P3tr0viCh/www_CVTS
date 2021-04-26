ab401ea13ce555c80e04725116d22dffcaa8aa9d
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2021-04-26 10:14:58 +0500

    2021.04.26

--- htdocs/include/MetaInfo.php
+++ htdocs/include/MetaInfo.php
@@ -5,6 +5,6 @@ class MetaInfo
     const AUTHOR = 'Дураев Константин Петрович (П3тр0виЧъ)';
     const COMPANY = 'Металлоинвест, Уральская Сталь';
 
-    const VERSION = '8.5.0';
-    const CREATION = '2021.04.22';
+    const VERSION = '8.6.0';
+    const CREATION = '2021.04.26';
 }
